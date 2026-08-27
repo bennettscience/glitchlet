7f43c28133096628afd5a32a4c59e6229a08ed10
Brian Bennett <brian.bennett2@gmail.com>
2026-08-27 19:34:09 -0400

    Cacheing, connectivity check events working

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -4,7 +4,7 @@
 On first load, cache all of the assets. From there, intercept fetch calls and respond appropriately with a cached resource or by falling back to a different response if the resource isn't available.
 */
 const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open("glitchlet-v1");
   await cache.put(request, response);
 };
 
@@ -32,16 +32,56 @@ const cacheFirst = async ({ request, fallbackUrl }) => {
     // Return a network error if there is no available fallback
     return new Response("Network error", {
       status: 408,
-      headers: { "Content=Type": "text/plain" },
+      headers: { "Content-Type": "text/plain" },
     });
   }
 };
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("online", handleConnection);
+self.addEventListener("offline", handleConnection);
+
+async function handleConnection() {
+  if (navigator.onLine) {
+    let status = await isReachable("https://example.com");
+    if (status) {
+      console.log("There is an active connection");
+    } else {
+      console.log("No connection");
+    }
+  } else {
+    console.log("Internet is disconnected");
+  }
+}
+
+async function isReachable(url) {
+  console.log("Trying to reach an address");
+  try {
+    let req = await fetch(url, { method: "HEAD", mode: "no-cors" });
+    console.log(req);
+    return req && (req.ok || req.type === "opaque");
+  } catch (err) {
+    console.warn("[connection test failure]: ", err);
+    return err;
+  }
+}
+
+self.navigation.addEventListener("navigate", (event) => {
+  console.log("Navigated: ", event);
+  event.preventDefault();
   event.respondWith(
     cacheFirst({
       request: event.request,
-      fallbackUrl: "/fallback.html",
+      fallbackUrl: "../../fallback.html",
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch requested: ", event);
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      fallbackUrl: "../../fallback.html",
     }),
   );
 });
